b8a5d566faeae74615c46cc051b5a7ce71e4ac2e
sergey280680 <sergey280680h@gmail.com>
2021-07-06 21:20:48 +0300

    task-2 done

--- src/js/_task.js
+++ src/js/_task.js
@@ -1,64 +1,63 @@
-const promise = new Promise((resolve, reject) => {
-  const canFullfill = Math.random() > 0.4;
+// const promise = new Promise((resolve, reject) => {
+//   const canFullfill = Math.random() > 0.4;
 
-  setTimeout(() => {
-    if (canFullfill) {
-      resolve('промис выполнился успешно с результатом (исполнен, Fullfill )');
-    }
+//   setTimeout(() => {
+//     if (canFullfill) {
+//       resolve('промис выполнился успешно с результатом (исполнен, Fullfill )');
+//     }
 
-    reject('промис выполнился с ошибкой (отклонен, rejected)');
-  }, 1000);
-});
+//     reject('промис выполнился с ошибкой (отклонен, rejected)');
+//   }, 1000);
+// });
 
-function onFullfield(result) {
-  console.log('onRejected => onRejected');
-  console.log(`${result}`);
-}
+// function onFullfield(result) {
+//   console.log('onRejected => onRejected');
+//   console.log(`${result}`);
+// }
 
-function onRejected(error) {
-  console.log('onRejected => onRejected');
-  console.log(`${error}`);
-}
+// function onRejected(error) {
+//   console.log('onRejected => onRejected');
+//   console.log(`${error}`);
+// }
 
-let fff = 'переменная fff';
+// let fff = 'переменная fff';
 
-promise
-  .then(result => {
-    console.log(result);
-    return fff;
-  })
-  .then(x => {
-    console.log(x);
-    return 10;
-  })
-  .then(y => {
-    console.log(y);
+// promise
+//   .then(result => {
+//     console.log(result);
+//     return fff;
+//   })
+//   .then(x => {
+//     console.log(x);
+//     return 10;
+//   })
+//   .then(y => {
+//     console.log(y);
 
+// // ==============================================
+// const makeOrder = dish => {
+//   const DELAY = 1000;
 
-// ==============================================
-const makeOrder = dish => {
-  const DELAY = 1000;
+//   return new Promise((resolve, reject) => {
+//     const passed = Math.random() > 0.4;
 
-  return new Promise((resolve, reject) => {
-    const passed = Math.random() > 0.4;
+//     setTimeout(() => {
+//       if (passed) {
+//         resolve(`вот ваш заказ ${dish}`);
+//       }
+//       reject(`на ваш заказ ${dish} закончились ингридиенты`);
+//     }, DELAY);
+//   });
+// };
 
-    setTimeout(() => {
-      if (passed) {
-        resolve(`вот ваш заказ ${dish}`);
-      }
-      reject(`на ваш заказ ${dish} закончились ингридиенты`);
-    }, DELAY);
-  });
-};
+// makeOrder('торт').then(onMakeOrderSuccess).catch(onMakeOrderError);
 
-makeOrder('торт').then(onMakeOrderSuccess).catch(onMakeOrderError);
+// function onMakeOrderSuccess(result) {
+//   console.log('onMakeOrderSuccess');
+//   console.log(result);
+// }
 
-function onMakeOrderSuccess(result) {
-  console.log('onMakeOrderSuccess');
-  console.log(result);
-}
-
-function onMakeOrderError(error) {
-  console.log('onMakeOrderError');
-  console.log(error);
-}
+// function onMakeOrderError(error) {
+//   console.log('onMakeOrderError');
+//   console.log(error);
+// }
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-// import './_task';
+import './_task';
 import './_task-1';
 import './_task-2';
 import './_task-3';
