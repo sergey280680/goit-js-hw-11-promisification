6a02d6cc2506850c68573751df85a2fe7d55b52b
sergey280680 <sergey280680h@gmail.com>
2021-07-13 23:27:04 +0300

    fixed read me

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-import './_task';
-import './_task-1';
-import './_task-2';
-import './_task-3';
+// import './_task';
+// import './_task-1';
+// import './_task-2';
+// import './_task-3';
+import './_sandra10.07.21';
